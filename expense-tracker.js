ca26569e5b6032befc25eac7cc2160ea03193dfd
zulal777 <zulaluzun777@gmail.com>
2024-03-25 17:51:51 +0000

    update

--- expense-tracker.js
+++ expense-tracker.js
@@ -1,5 +1,3 @@
-//? Selectors
-
 const ekleBtn = document.getElementById("ekle-btn");
 const gelirInput = document.getElementById("gelir-input");
 const ekleFormu = document.getElementById("ekle-formu");
@@ -8,23 +6,17 @@ const gelirinizTd = document.getElementById("geliriniz");
 const giderinizTd = document.getElementById("gideriniz");
 const kalanTd = document.getElementById("kalan");
 
-//& Variables
-
 let gelirler = 0;
 let harcamaListesi = [];
 
-//* Ekle Formu
 ekleFormu.addEventListener("submit", (e) => {
-  e.preventDefault(); //! reload u engellemek için
+  e.preventDefault();
   gelirler = gelirler + Number(gelirInput.value);
   ekleFormu.reset();
-  // console.log(gelirler)
+
   localStorage.setItem("gelirler", gelirler);
   hesaplaVeGuncelle();
-  // gelirinizTd.textContent = gelirler
 });
-
-//! Harcama Formu
 
 const harcamaFormu = document.getElementById("harcama-formu");
 const tarihInput = document.getElementById("tarih");
@@ -35,7 +27,7 @@ const harcamaBody = document.getElementById("harcama-body");
 const temizleBtn = document.getElementById("temizle-btn");
 
 harcamaFormu.addEventListener("submit", (e) => {
-  e.preventDefault(); //! reload u engellemek için
+  e.preventDefault();
   const yeniHarcama = {
     tarih: tarihInput.value,
     miktar: miktarInput.value,
@@ -43,9 +35,8 @@ harcamaFormu.addEventListener("submit", (e) => {
     id: new Date().getTime(),
   };
 
-  // console.log(yeniHarcama)
   harcamaListesi.push(yeniHarcama);
-  // console.log(harcamaListesi)
+
   localStorage.setItem("harcamalar", JSON.stringify(harcamaListesi));
   harcamayiDomaYaz(yeniHarcama);
 
@@ -54,27 +45,7 @@ harcamaFormu.addEventListener("submit", (e) => {
   hesaplaVeGuncelle();
 });
 
-//& Harcamayı Dom'a yaz
-
 const harcamayiDomaYaz = ({ id, miktar, tarih, alan }) => {
-  // const {id, miktar, tarih, alan} = yeniHarcama
-  // console.log(id, miktar, tarih, alan)
-
-  //! 1. yöntem (Eski ve güvenlik açığı nedeniyle kullanılmaması gereken)
-  // harcamaBody.innerHTML += `
-  //     <tr>
-  //         <td>${tarih}</td>
-  //         <td>${alan}</td>
-  //         <td>${miktar}</td>
-  //         <td><i id=${id} class="fa-solid fa-trash-can text-danger"  type="button"></i></td>
-  //     </tr>
-
-  // `
-  //^ innerHTML kullandığımızda aşağıdaki kodları harcama alanına yazarsak istemediğimiz şekilde bir tablo düzeni ile karşılaşırız.
-  //^ <h2>Hello World!</h2><p>Have a nice day!</p>
-  //^ <img src="https://5.imimg.com/data5/VK/EK/UG/SELLER-101818061/danger-signs-500x500.jpg">
-
-  //! 2. yöntem
   const tr = document.createElement("tr");
 
   const appendTd = (content) => {
@@ -94,18 +65,10 @@ const harcamayiDomaYaz = ({ id, miktar, tarih, alan }) => {
     return td;
   };
 
-  tr.append(
-    appendTd(tarih), //tarih td si
-    appendTd(alan), //alan td si
-    appendTd(miktar), //miktar td si
-    createLastTd() // Çöp kutusu ve id yi ekler
-  );
+  tr.append(appendTd(tarih), appendTd(alan), appendTd(miktar), createLastTd());
 
-  harcamaBody.append(tr); //& son girileni alta ekler
-  // harcamaBody.prepend(tr) //& son girileni öne ekler
+  harcamaBody.append(tr);
 };
-
-//& hesaplaVeGuncelle
 
 const hesaplaVeGuncelle = () => {
   const giderler = harcamaListesi.reduce(
@@ -113,17 +76,15 @@ const hesaplaVeGuncelle = () => {
     0
   );
 
-  // console.log(giderler)
+  console.log(giderler);
 
   giderinizTd.textContent = giderler;
   gelirinizTd.textContent = gelirler;
   kalanTd.textContent = gelirler - giderler;
 };
 
-//? Event listeners
-
 window.addEventListener("load", () => {
-  gelirler = Number(localStorage.getItem("gelirler")) || 0; //! Local storage den gelirleri al numberlaştır. Veri yoksa 0 ata
+  gelirler = Number(localStorage.getItem("gelirler")) || 0;
   harcamaListesi = JSON.parse(localStorage.getItem("harcamalar")) || [];
 
   harcamaListesi.forEach((harcama) => {
@@ -137,13 +98,11 @@ window.addEventListener("load", () => {
 });
 
 harcamaBody.addEventListener("click", (e) => {
-  // console.log(e.target.classList.contains("fa-trash-can"))
-
   if (e.target.classList.contains("fa-trash-can")) {
     e.target.parentElement.parentElement.remove();
     const id = e.target.id;
     harcamaListesi = harcamaListesi.filter((harcama) => harcama.id != id);
-    // console.log(harcamaListesi)
+
     localStorage.setItem("harcamalar", JSON.stringify(harcamaListesi));
   }
 });
@@ -153,7 +112,7 @@ temizleBtn.addEventListener("click", () => {
     harcamaListesi = [];
     gelirler = 0;
     harcamaBody.innerHTML = "";
-    // localStorage.clear() // Tüm localstorage i siler
+
     localStorage.removeItem("gelirler");
     localStorage.removeItem("harcamalar");
     hesaplaVeGuncelle();
